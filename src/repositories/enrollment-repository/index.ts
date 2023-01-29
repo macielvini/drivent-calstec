47001dc1b0f78721436471a07cb745a3b733b016
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-28 22:19:41 -0300

    feat: find user enrollment

--- src/repositories/enrollment-repository/index.ts
+++ src/repositories/enrollment-repository/index.ts
@@ -24,12 +24,17 @@ async function upsert(
   });
 }
 
+async function findUserEnrollment(id: number) {
+  return await prisma.enrollment.findFirst({ where: { userId: id } });
+}
+
 export type CreateEnrollmentParams = Omit<Enrollment, "id" | "createdAt" | "updatedAt">;
 export type UpdateEnrollmentParams = Omit<CreateEnrollmentParams, "userId">;
 
 const enrollmentRepository = {
   findWithAddressByUserId,
   upsert,
+  findUserEnrollment,
 };
 
 export default enrollmentRepository;
