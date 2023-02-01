ebce9b1b9e9b6ff806d92ab96e7bf8c62ce24075
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-02-01 16:33:00 -0300

    feat: validate if user has paid ticket with hotel

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -4,11 +4,12 @@ import httpStatus from "http-status";
 import { hotelServices } from "@/services/hotels-service";
 
 export async function findAll(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
   try {
-    const data = await hotelServices.findAll();
+    const data = await hotelServices.findAll(Number(userId));
     res.send(data);
   } catch (error) {
-    res.sendStatus(500);
+    return res.send(error) || res.sendStatus(500);
   }
 }
 
--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,7 +1,25 @@
+import enrollmentRepository from "@/repositories/enrollment-repository";
 import { hotelRepository } from "@/repositories/hotel-repository";
+import ticketRepository from "@/repositories/ticket-repository";
+import { TicketStatus } from "@prisma/client";
+import httpStatus from "http-status";
 
-async function findAll() {
+async function findAll(userId: number) {
+  await hasPaidTicketWithHotel(userId);
   return await hotelRepository.findAll();
+}
+
+async function hasPaidTicketWithHotel(userId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+
+  if (!enrollment || !ticket) throw httpStatus.NOT_FOUND;
+
+  const includesHotel = ticket.TicketType.includesHotel;
+  const isPaid = ticket.status === TicketStatus.PAID;
+  const isRemote = ticket.TicketType.isRemote;
+
+  if (!includesHotel || !isPaid || isRemote) throw httpStatus.PAYMENT_REQUIRED;
 }
 
 export const hotelServices = { findAll };
