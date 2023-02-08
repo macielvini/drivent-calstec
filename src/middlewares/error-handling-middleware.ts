a341c4cdf2d1d3bb7102d7e3e7023363a398617b
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-02-07 22:34:39 -0300

    feat: added payment required to error handler

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -15,6 +15,12 @@ export function handleApplicationErrors(err: ApplicationError | Error, _req: Req
     });
   }
 
+  if (err.name === "PaymentRequired") {
+    return res.status(httpStatus.PAYMENT_REQUIRED).send({
+      message: err.message,
+    });
+  }
+
   if (err.name === "InvalidCredentialsError") {
     return res.status(httpStatus.UNAUTHORIZED).send({
       message: err.message,
