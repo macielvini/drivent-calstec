ad83eb96332009d3537300f1c267116256385ff1
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-02-07 22:35:36 -0300

    refactor: error handling

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -1,6 +1,5 @@
-import { AuthenticatedRequest } from "@/middlewares";
+import { AuthenticatedRequest, handleApplicationErrors } from "@/middlewares";
 import { Response } from "express";
-import { notFoundError } from "@/errors";
 
 import bookingService from "@/services/booking-service";
 
@@ -8,17 +7,13 @@ export async function bookingPost(req: AuthenticatedRequest, res: Response) {
   const userId = Number(req.userId);
   const roomId = Number(req.body.roomId);
 
-  if (!roomId) throw notFoundError();
-
   try {
+    if (isNaN(roomId)) return res.status(422).send({ message: "roomId is required" });
+
     const data = await bookingService.createOne(userId, roomId);
 
     res.status(200).send({ bookingId: data.id });
   } catch (error) {
-    if (error.name === "NotFoundError") return res.status(404).send(error);
-    if (error.name === "RoomIsFullError") return res.status(403).send(error);
-    if (error.name === "PaymentRequired") return res.status(402).send(error);
-
-    return res.sendStatus(500);
+    handleApplicationErrors(error, req, res);
   }
 }
